83d6ab5c7961273d80a558f695c424245a406018
rachel-howell <rhowell96@gmail.com>
2023-03-15 21:05:50 -0700

    Lay foundation for watch builder component

---
+++ components/WatchBuilder.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const WatchBuilder = () => {
+  return (
+    <div className="flex flex-row">
+        <div>
+            {/* Watch Image Area Here */}
+        </div>
+        <div>
+            <h1>Watch Title</h1>
+            <p>Price</p>
+            <p>Dial</p>
+            <p>Case</p>
+            <p>Hands</p>
+            <p>Strap</p>
+            <button>Add to Cart</button>
+        </div>
+    </div>
+  )
+}
+
+export default WatchBuilder
