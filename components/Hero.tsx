78c546d7c00a86c755ca4faf613920b0d1014e5f
rachel-howell <rhowell96@gmail.com>
2023-03-28 18:51:04 -0700

    Add image to Hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
+    <div>
+    <div className="flex flex-row justify-center mt-5">
+      <Image src={require('../assets/hero.png')} className="w-screen" alt="rls logo"/> 
+    </div>
     <div className="w-full h-96 bg-black mt-8 text-white flex flex-col items-center justify-center">
+
       <div className="max-w-5xl mx-auto text-center">
         <p className="text-6xl mb-4 sourceserifpro">Custom Watch Mods</p>
         <p className="text-2xl mb-5">Indulge in the world of personalized design and customization with our hand-built custom watches. Every timepiece is crafted with meticulous attention to detail, ensuring that each watch is as unique as its owner.</p>
         <p className="text-sm italic text-gray-500">RLSWW is not affiliated with any brands.</p>
       </div>
     </div>
+  </div>
   )
 }
 
